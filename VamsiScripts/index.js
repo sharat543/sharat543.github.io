d60af5e249d253e34918c9ee5eaa4e6901c705cc
sharat543 <savaranasi@deloitte.com>
2017-11-08 16:40:37 +0530

    Update index.js

--- VamsiScripts/index.js
+++ VamsiScripts/index.js
@@ -6,7 +6,7 @@ var input;
 var voices = speechSynthesis.getVoices();
 var isPlatinum = false;
 var mobileNo;
-var complaint;
+var dashboardName;
 var response;
 
 // On doument load resolve the SDK dependecy
@@ -15,9 +15,7 @@ var response;
             require(["Speech.Browser.Sdk"], function(SDK) {
                 onComplete(SDK);
 				$("#mobileNo").prop("disabled", true);
-				voiceoutput("Tell me, how can I assist you?")
-				statusImage.innerHTML = "";
-				statusImage.innerHTML = "<img src='mic.png'/>";	
+				voiceoutput("Welcome. Provide the dashboard name that you want to view.")
 				if (!recognizer || previousSubscriptionKey != key.value) {
                     previousSubscriptionKey = key.value;
                     Setup();
@@ -82,7 +80,7 @@ var response;
                     case "RecognitionEndedEvent" :
                         OnComplete();
                         console.log(JSON.stringify(event)); // Debug information
-						submitComplaint($('#hypothesisDiv').text());
+						openDashboard($('#hypothesisDiv').text());
 						//alert($('#hypothesisDiv').text());
                         break;
                 }
@@ -158,90 +156,13 @@ var response;
             stopBtn.disabled = true;
         }
 	
-
-
-// Numeric only control handler
-jQuery.fn.ForceNumericOnly =
-function()
-{
-    return this.each(function()
-    {
-        $(this).keydown(function(e)
-        {
-            var key = e.charCode || e.keyCode || 0;
-            // allow backspace, tab, delete, enter, arrows, numbers and keypad numbers ONLY
-            // home, end, period, and numpad decimal
-            return (
-                key == 8 || 
-                key == 9 ||
-                key == 13 ||
-                key == 46 ||
-                key == 110 ||
-                key == 190 ||
-                (key >= 35 && key <= 40) ||
-                (key >= 48 && key <= 57) ||
-                (key >= 96 && key <= 105));
-        });
-    });
-};
-
-$('#mobileNo').keypress(function (e) {
-  if (e.which == 35 && $('#mobileNo').val().length == 10) {	
-	isPlatinum = false;
-	statusImage = document.getElementById("statusImage");
-	statusImage.innerHTML = "<img src='brain.jpg' id='blinkImage'/>";
-	voiceoutput("Please wait, while we search your mobile number.");
-	$("#mobileNo").prop("disabled", true);
-	window.setTimeout(function(){
-	mobileNo = $('#mobileNo').val();
-	$("#mobileNo").prop("disabled", true);
-	if(mobileNo == "9000012345" || mobileNo == "9000012346" || mobileNo == "9000012347"){
-			if(mobileNo == "9000012345"){
-				isPlatinum = true;				
-				voiceoutput("Welcome John, to intelligent ticket routing system. Please wait while your request is being processed.");	
-				calculateComplaintPriority();
-				console.log(response);
-				recordComplaint(response.priority);
-			}
-			else if(mobileNo == "9000012346") {					
-				voiceoutput("Welcome Alex, to intelligent ticket routing system. Please wait while your request is being processed.");
-				calculateComplaintPriority();
-				console.log( response);
-				recordComplaint( response.priority);	
-			}
-			else if(mobileNo == "9000012347") {	
-				voiceoutput("Welcome Mark, to intelligent ticket routing system. Please wait while your request is being processed.");
-				calculateComplaintPriority();
-				console.log( response);
-				recordComplaint( response.priority);	
-			}
-			
-		}else{
-			statusImage.innerHTML = "";
-			$('#mobileNo').val("");
-			$("#formArea").css({"display":"block"});
-			$("#mobileNo").prop("disabled", false);
-			voiceoutput("Sorry, kindly check you mobile number.");
-		}
-	
-	//$("#formArea").css({"display:none"});
-    return false;    //<---- Add this line
-	}, 5000 );
-  }
-});
-
 function submitComplaint(data){
-	complaint = data;
-	if(data != ""){
-		voiceoutput('Please enter your mobile number followed by the hash key for confirming your details');	
-		$("#mobileNo").prop("disabled", false);
-		statusImage = document.getElementById("statusImage");	
-		statusImage.innerHTML = "";
+	dashboardName = data;
+	if(dashboardName != ""){
+		voiceoutput("Loading your dashboard.Please wait.....")
 	}
 	else{
-		voiceoutput("Tell me, how can I assist you?")
-		statusImage.innerHTML = "";
-		statusImage.innerHTML = "<img src='mic.png'/>";	
+		voiceoutput("Provide the dashboard name that you want to view.")
 		/*if (!recognizer || previousSubscriptionKey != key.value) {
 			previousSubscriptionKey = key.value;
             Setup();
@@ -249,57 +170,7 @@ function submitComplaint(data){
 		RecognizerStart(SDK, recognizer);
 	}
 }
-
-function recordComplaint(tier){
-	statusImage = document.getElementById("statusImage");
-	statusImage.innerHTML = "<img src='calculate.jpg' id='blinkImage'/>";
 	
-	window.setTimeout(function(){
-	if ( tier == 1)
-	{
-	voiceoutput("Your case has been assigned to our tier 1 support executive. You will be assisted shortly. Here's your case number");
-	}
-	else if (tier == 2)
-	{
-	voiceoutput("A case has been created and You will be contacted shortly. Here's your case number");
-	}
-	else{
-	voiceoutput("Your case has been assigned to our tier 2 support executive. You will be assisted shortly. Here's your case number");
-	}
-	statusImage.innerHTML = "<img src='call.jpg' id='blinkImage'/>";
-	}, 9000 );
-}
-
-
-
-$("#mobileNo").ForceNumericOnly();
-
-function voiceoutput(text){
-
-            if('speechSynthesis' in window){
-			var speech = new SpeechSynthesisUtterance(text);
-			speech.lang = 'en-US';
-			speech.voice = voices[4]
-			window.speechSynthesis.speak(speech);			
-           
-			}
-        } 
-		
-		
-function calculateComplaintPriority(){	
-	var data = {mobileNo:mobileNo,complaint:complaint}
-	$.ajax({
-    url: 'http://127.0.0.1:5000/registerComplaint',
-    type: 'POST',
-    data: JSON.stringify(data),
-    contentType: 'application/json; charset=utf-8',
-    dataType: 'json',
-    async: false,
-    success: function(msg) {
-		response = msg;
-    }
-});
-}	
 		
 /*function blink(){
     $('#blinkImage').delay(100).fadeTo(100,0.5).delay(100).fadeTo(100,1, blink);
